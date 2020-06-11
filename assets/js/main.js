3252af419bf6e1c6e777869b6da15a559be873f2
MANOJ HANSDA <mail@manojhansda.com>
2020-06-11 23:27:36 +0530

    toolbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -905,14 +905,27 @@
     =====================================*/
     var $demoOption = $('.demo-option-container')
 
+    $(".support-container").hide();
+    $(".share-container").show();
 
     $('.quick-option').on('click', function (e) {
         e.preventDefault(),
         function () {
+            $(".share-container").hide();
+            $(".support-container").show();
             $demoOption.toggleClass('open')
         }()
     });
 
+
+    $('.quick-option-share').on('click', function (e) {
+        e.preventDefault(),
+        function () {
+            $(".support-container").hide();
+            $(".share-container").show();
+            $demoOption.toggleClass('open')
+        }()
+    });
 
     /*=============================================
     =            counter up active            =
